336edec75bce353cedda2b233ded8ad869ba75ea
Sandra Lindström <sandra.lindstrm@gmail.com>
2021-08-23 16:12:31 +0200

    first commit

---
+++ src/AddTodoForm.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import { Plus } from 'react-bootstrap-icons';
+
+interface Props {
+ addTodo: AddTodo 
+}
+export const AddTodoForm: React.FC<Props> = ({addTodo}) => {
+  const [text, setText] = useState('');
+
+  return (
+    <form>
+      <input type="text"
+      className='todo-item-input'
+      value={text}
+      onChange={e=> {setText(e.target.value)}} />
+      <button type="submit"
+      onClick={e=> {e.preventDefault();
+      addTodo(text);
+      setText('')}
+      }><Plus size={35}/></button>
+
+    </form>
+  );
+};
